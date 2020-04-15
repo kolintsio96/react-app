006a24da111336d45a29cf8048d10ce388d81479
vkolintso <v.kolintso@theomnicore.com>
2020-04-15 18:58:10 +0300

    react lazy && react suspense

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {Route} from 'react-router-dom'
 import style from './index.module.scss';
-import MessagesContainer from "./messages/messagesContainer";
-import ProfileContainer from "./profile/profileContainer";
 import UsersContainer from "./users/usersContainer";
-import Login from "./login/loginContainer";
+import withSuspense from "../../hoc/withSuspense";
+const ProfileContainer = React.lazy(() => import('./profile/profileContainer'));
+const MessagesContainer = React.lazy(() => import('./messages/messagesContainer'));
+const Login = React.lazy(() => import('./login/loginContainer'));
 const Content = () => {
     return (
             <div className={style.content}>
-                <Route path = '/profile/:userId?' render={() => <ProfileContainer/>}/>
-                <Route path = '/messages' render={() => <MessagesContainer/>}/>
+                <Route path = '/profile/:userId?' render={withSuspense(ProfileContainer)}/>
+                <Route path = '/messages' render={withSuspense(MessagesContainer)}/>
                 <Route path = '/users' render={() => <UsersContainer/>}/>
-                <Route path = '/login' render={() => <Login/>}/>
+                <Route path = '/login' render={withSuspense(Login)}/>
             </div>
     )
 }
